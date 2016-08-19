f2a75a5718da54299a206e42989578ea7fb27d3f
Terry_Liu <tkuterry.liu@gmail.com>
2016-08-20 07:28:39 +0800

    change the order of class member function

--- pyws/pwclient.py
+++ pyws/pwclient.py
@@ -15,23 +15,6 @@ class PWClient:
         self.__wsVersion = _WS_IMPLEMENTED_VERSION
         self.__handhakeDic = {'Connection': 'Upgrade', 'Upgrade': 'websocket', 'Sec-WebSocket-Version': self.__wsVersion}
 
-    def __create_handshake(self):
-        #TODO: change request-URI
-        handshake_msg = 'GET / HTTP/1.1\n'
-        for key, value in self.__handhakeDic.iteritems() :
-            handshake_msg += key + ': ' + value + '\r\n'
-        handshake_msg += '\r\n'
-        print 'handshake_msg: ' + handshake_msg
-        return handshake_msg
-
-    def __produce_websocket_key(self):
-        ran_bytes = os.urandom(_WSKEY_RANDOM_BYTES_SIZE)
-        key_str = ran_bytes.encode('base64')
-        return str.rstrip(key_str, '\n')
-
-    def set_header(self, key, value):
-        self.__handhakeDic[key] = value
-
     def start_connection(self, url, port):
         'connect to server and send HTTP handshake'
 
@@ -53,3 +36,20 @@ class PWClient:
 
         #recv_bytes = self.__sock.recv()
         #print recv_bytes
+
+    def set_header(self, key, value):
+        self.__handhakeDic[key] = value
+
+    def __create_handshake(self):
+        #TODO: change request-URI
+        handshake_msg = 'GET / HTTP/1.1\n'
+        for key, value in self.__handhakeDic.iteritems() :
+            handshake_msg += key + ': ' + value + '\r\n'
+        handshake_msg += '\r\n'
+        print 'handshake_msg: ' + handshake_msg
+        return handshake_msg
+
+    def __produce_websocket_key(self):
+        ran_bytes = os.urandom(_WSKEY_RANDOM_BYTES_SIZE)
+        key_str = ran_bytes.encode('base64')
+        return str.rstrip(key_str, '\n')
