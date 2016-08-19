e5d58cc8dc3cdf0f6070194df3d7fdb724271b82
Terry_Liu <tkuterry.liu@gmail.com>
2016-08-20 07:35:48 +0800

    socket instance should be private

--- pyws/pwsocket.py
+++ pyws/pwsocket.py
@@ -6,21 +6,21 @@ import socket
 class PWSocket:
     'The class which wraps the socket module and provides basic socket operations'
     def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def __del__(self):
         print 'closing socket...'
-        self.close()
+        self.__sock.close()
 
     def connect(self, addr, port):
         print 'connecting to ' + addr + ':' + str(port)
-        self.sock.connect((addr, port))
+        self.__sock.connect((addr, port))
 
     def send(self, data):
-        self.sock.send(data)
+        self.__sock.send(data)
 
     def recv(self, bufsize=1024):
-        return self.sock.recv(bufsize)
+        return self.__sock.recv(bufsize)
 
     def close(self):
-        self.sock.close()
+        self.__sock.close()
